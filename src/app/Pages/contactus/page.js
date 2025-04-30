de55f2fa96d5fcac970a39292a07f288f06d8a7e
Gayashan Maduwantha <gayak3088@gmail.com>
2025-04-30 19:13:00 +0530

    change name2

--- src/app/Pages/contactus/page.js
+++ src/app/Pages/contactus/page.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
+
 import Image from 'next/image';
 
 export default function Contact() {
